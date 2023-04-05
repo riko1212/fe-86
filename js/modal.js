ac5f0caf74be55bad4aa404e890c93da2f36f123
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-04-05 21:40:04 +0300

    mod-5

---
+++ js/modal.js
@@ -0,0 +1,26 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.forEach((element) => {
+    element.addEventListener('click', toggleModal);
+  });
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  //* Закривання по кліку на бекдроп
+
+  // refs.modal.addEventListener('click', removeModal);
+  // function removeModal(e) {
+  //   if (e.target === e.currentTarget) {
+  //     refs.modal.classList.add('is-hidden');
+  //   }
+  // }
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
+})();
